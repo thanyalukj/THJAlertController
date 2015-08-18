cf5469dfc34f6304539fe9f20795df9dbbf78811
Thanyaluk Jirapech-umpai <thanyaluk.jirapech-umpai@kotikan.com>
2015-08-18 23:34:59 +0100

    stop tracking unnecessary files.

--- Example/Pods/Target Support Files/Pods-THJAlertController_Tests/Pods-THJAlertController_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_THJAlertController_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_THJAlertController_Tests
-@end
--- Example/Pods/Target Support Files/THJAlertController/THJAlertController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_THJAlertController : NSObject
-@end
-@implementation PodsDummy_THJAlertController
-@end
